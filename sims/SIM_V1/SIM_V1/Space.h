7e9e8ebc5dae9086f6980ad3ff4fdc84a326a727
Garand Tyson <gttyson@stanford.edu>
2019-01-28 00:15:31 -0800

    Added documentation for space.h

--- sims/SIM_V1/SIM_V1/Space.h
+++ sims/SIM_V1/SIM_V1/Space.h
@@ -1,3 +1,14 @@
+/*
+  Stanford Student Space Initiative
+  Satellites | Sims | December 2018
+  Garand Tyson  | gttyson@stanford.edu
+
+  File: Space.h
+  --------------------------
+  Space is a data structure to hold all information for a given simulations,
+  including simulation time interval, component lists, entity information, etc.
+*/
+
 #pragma once
 
 #include "coreinclude.h"
@@ -6,14 +17,19 @@ namespace EE {
 	class Space
 	{
 	private:
-		eId nextId;
+/************************************  PROPERTIES *************************************/
+
+		/* Simulation time information */
 		timeUnit time;
 		timeUnit dt;
 
+		/* Entity information */
+		eId nextId;
 		eIdToName idToName;
 		eNameToId nameToId;
 		eIdToPointer idToPointer;
 
+		/* System Information */
 		sysNameVec sysUpdateOrder;
 		sNameToPtr sNameToPointer;
 
